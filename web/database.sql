1be8bc37a6e14d643fcb920089e0026fd049e54e
Matthieu CLIN <16208312+clinm@users.noreply.github.com>
2013-12-03 22:51:22 +0100

    maj bdd

--- web/database.sql
+++ web/database.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.10.1deb1
+-- version 4.0.4
 -- http://www.phpmyadmin.net
 --
--- Client: 195.83.142.10:3306
--- Généré le : Mar 03 Décembre 2013 à 18:49
--- Version du serveur: 5.5.34
--- Version de PHP: 5.3.10-1ubuntu3.8
+-- Client: localhost
+-- Généré le: Mar 03 Décembre 2013 à 21:50
+-- Version du serveur: 5.6.12-log
+-- Version de PHP: 5.4.16
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,8 +17,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données: `clin1u_bourse_livre`
+-- Base de données: `bourse_livre`
 --
+CREATE DATABASE IF NOT EXISTS `bourse_livre` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `bourse_livre`;
 
 -- --------------------------------------------------------
 
@@ -29,8 +31,19 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `associations` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `mail` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `associations`
+--
+
+INSERT INTO `associations` (`id`, `name`, `address`, `phone`, `mail`) VALUES
+(1, 'P.E.E.P', '89/91 Boulevard Berthier 75017 Paris', '0606060606', 'peep@peep.fr'),
+(2, 'F.C.P.E', '108-110 avenue Ledru-Rollin 75544 Paris cedex 11', '01.43.57.16.16', 'fcpe@fcpe.fr');
 
 -- --------------------------------------------------------
 
@@ -50,6 +63,23 @@ CREATE TABLE IF NOT EXISTS `books` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `clients`
+--
+
+CREATE TABLE IF NOT EXISTS `clients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `mail` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `association_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `association_id` (`association_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `conditions`
 --
 
@@ -58,7 +88,16 @@ CREATE TABLE IF NOT EXISTS `conditions` (
   `name` varchar(255) NOT NULL,
   `reducing` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `conditions`
+--
+
+INSERT INTO `conditions` (`id`, `name`, `reducing`) VALUES
+(1, 'Bon', 5),
+(2, 'Moyen', 10),
+(3, 'MÃ©diocre', 15);
 
 -- --------------------------------------------------------
 
@@ -72,24 +111,18 @@ CREATE TABLE IF NOT EXISTS `grades` (
   `sector_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `sector_id` (`sector_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
--- Structure de la table `parents`
+-- Contenu de la table `grades`
 --
 
-CREATE TABLE IF NOT EXISTS `parents` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `mail` varchar(255) NOT NULL,
-  `phone` varchar(255) NOT NULL,
-  `association_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `association_id` (`association_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+INSERT INTO `grades` (`id`, `name`, `sector_id`) VALUES
+(1, '1Ã¨re', 1),
+(2, 'Terminale', 1),
+(3, '1Ã¨re', 2),
+(4, 'Terminale', 1),
+(5, '1Ã¨re', 3);
 
 -- --------------------------------------------------------
 
@@ -133,7 +166,19 @@ CREATE TABLE IF NOT EXISTS `sectors` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Contenu de la table `sectors`
+--
+
+INSERT INTO `sectors` (`id`, `name`) VALUES
+(1, 'SSVT'),
+(2, 'L'),
+(3, 'STL'),
+(4, 'STI'),
+(5, 'SSI'),
+(6, 'ES');
 
 -- --------------------------------------------------------
 
@@ -147,7 +192,18 @@ CREATE TABLE IF NOT EXISTS `subjects` (
   `grade_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `grade_id` (`grade_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Contenu de la table `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `name`, `grade_id`) VALUES
+(1, 'MathÃ©matique', 1),
+(2, 'Allemand', 2),
+(3, 'Anglais', 3),
+(4, 'FranÃ§ais', 5),
+(5, 'Physique', 4);
 
 -- --------------------------------------------------------
 
@@ -162,8 +218,10 @@ CREATE TABLE IF NOT EXISTS `transactions` (
   `paymentMethod` varchar(255) NOT NULL,
   `type` varchar(255) NOT NULL,
   `close` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`)
+  KEY `user_id` (`user_id`),
+  KEY `client_id` (`client_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -178,7 +236,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `status`) VALUES
+(1, 'root', '577ea4df223bed7d8e6ab6d099eb3cbfbc00895a', 'admin');
 
 --
 -- Contraintes pour les tables exportées
@@ -191,24 +256,24 @@ ALTER TABLE `books`
   ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`);
 
 --
+-- Contraintes pour la table `clients`
+--
+ALTER TABLE `clients`
+  ADD CONSTRAINT `clients_ibfk_1` FOREIGN KEY (`association_id`) REFERENCES `associations` (`id`);
+
+--
 -- Contraintes pour la table `grades`
 --
 ALTER TABLE `grades`
   ADD CONSTRAINT `grades_ibfk_1` FOREIGN KEY (`sector_id`) REFERENCES `sectors` (`id`);
 
 --
--- Contraintes pour la table `parents`
---
-ALTER TABLE `parents`
-  ADD CONSTRAINT `parents_ibfk_1` FOREIGN KEY (`association_id`) REFERENCES `associations` (`id`);
-
---
 -- Contraintes pour la table `records`
 --
 ALTER TABLE `records`
-  ADD CONSTRAINT `records_ibfk_3` FOREIGN KEY (`condition_id`) REFERENCES `conditions` (`id`),
   ADD CONSTRAINT `records_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
-  ADD CONSTRAINT `records_ibfk_2` FOREIGN KEY (`transaction_id`) REFERENCES `transactions` (`id`);
+  ADD CONSTRAINT `records_ibfk_2` FOREIGN KEY (`transaction_id`) REFERENCES `transactions` (`id`),
+  ADD CONSTRAINT `records_ibfk_3` FOREIGN KEY (`condition_id`) REFERENCES `conditions` (`id`);
 
 --
 -- Contraintes pour la table `rows`
@@ -226,6 +291,7 @@ ALTER TABLE `subjects`
 -- Contraintes pour la table `transactions`
 --
 ALTER TABLE `transactions`
+  ADD CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`),
   ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
