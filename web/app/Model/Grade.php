a57b7f7bcc9ccaa114b85634a7fc160f2cbb3727
Matthieu CLIN <16208312+clinm@users.noreply.github.com>
2013-12-18 10:47:25 +0100

    formualaire classe c?t? model

--- web/app/Model/Grade.php
+++ web/app/Model/Grade.php
@@ -1,5 +1,26 @@
 <?php 
 class Grade extends AppModel{
 	public $belongsTo = array('Sector');
+
+	public $validate = array(
+			'name' => array(
+				'unique' => array(
+						'rule' => array('isUniqueBy', 'sector_id'),
+						'required' => true,
+						'message' => 'Le nom de la classe doit être unique pour une filière donnée'
+					),
+				'correct' => array(
+						'rule' => '/^[a-z]{3,}$/i',
+						'message' => 'Le nom de la classe doit uniquement contenir des lettres'
+					)
+				)
+		);
+
+
+	public function isUniqueBy($options = array(), $value) {
+		$key = array_keys($options);
+		return !$this->find('count', array('conditions' => array($this->alias.'.'.current($key) => $options[current($key)],
+															 	$this->alias.'.'.$value => $this->data[$this->alias][$value])));
+	}
 }
  ?>
