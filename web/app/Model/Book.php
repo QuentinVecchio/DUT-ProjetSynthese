3d796c2a6158d7591477aea1771059e2b1f240b4
Matthieu CLIN <16208312+clinm@users.noreply.github.com>
2013-12-23 10:19:38 +0100

    protection CakePHP pour les livres

--- web/app/Model/Book.php
+++ web/app/Model/Book.php
@@ -1,5 +1,31 @@
 <?php 
 class Book extends AppModel{
 	public $belongsTo = array('Subject');
+
+	public $validate = array(
+			'name' => array(
+				'unique' => array(
+						'rule' => array('isUniqueBy', 'subject_id'),
+						'required' => true,
+						'message' => 'Le nom du livre doit être unique pour une matière donnée'
+					),
+				'correct' => array(
+						'rule' => '/^[a-z]{3,}$/i',
+						'message' => 'Le nom du livre doit uniquement contenir des lettres'
+					)
+				),
+			'prize' => array(
+				'rule' => array('comparison', '>', 0),
+				'message' => 'Un prix est toujours strictement positif',
+				'required' => true)
+		);
+
+
+	public function isUniqueBy($options = array(), $value) {
+		$key = array_keys($options);
+		return !$this->find('count', array('conditions' => array($this->alias.'.'.current($key) => $options[current($key)],
+															 	$this->alias.'.'.$value => $this->data[$this->alias][$value])));
+	}
+
 }
  ?>
