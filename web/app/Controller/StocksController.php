64d474fc9efed198f1333b49bb2fb36ead6ac50b
Matthieu CLIN <16208312+clinm@users.noreply.github.com>
2014-01-04 21:14:19 +0100

    affichage stock debug

--- web/app/Controller/StocksController.php
+++ web/app/Controller/StocksController.php
@@ -3,6 +3,12 @@ class StocksController extends AppController{
 
 	public function admin_index(){
 		
+		$this->loadModel('Transaction');
+		$stock = $this->Transaction->Row->find('all',array( 'fields' => array('Transaction.date', 'book_id', 'condition_id','SUM(amount) AS amount'),
+															'condition' =>array('type' => 'depot'),
+															'group' => array('Transaction.date','book_id','condition_id')
+															));
+		$this->set('stock', $stock);
 	}
 
 }
