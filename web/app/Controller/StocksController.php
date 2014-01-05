b551bf1110480c17b1061fc5c8b59136e1776420
Matthieu CLIN <16208312+clinm@users.noreply.github.com>
2014-01-05 10:17:50 +0100

    modif requete pour stock

--- web/app/Controller/StocksController.php
+++ web/app/Controller/StocksController.php
@@ -4,9 +4,10 @@ class StocksController extends AppController{
 	public function admin_index(){
 		
 		$this->loadModel('Transaction');
-		$stock = $this->Transaction->Row->find('all',array( 'fields' => array('Transaction.date', 'book_id', 'condition_id','SUM(amount) AS amount'),
-															'condition' =>array('type' => 'depot'),
-															'group' => array('Transaction.date','book_id','condition_id')
+		$stock = $this->Transaction->Row->find('all',array( 'fields' => array('Transaction.date', 'book_id',
+																			  'Transaction.type', 'Condition.name',
+																			  'condition_id','SUM(amount) AS amount',),
+															'group' => array('Transaction.date','type', 'book_id','condition_id')
 															));
 		$this->set('stock', $stock);
 	}
